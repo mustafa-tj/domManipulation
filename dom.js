43a72ea9183d8693c317707a373c0089629362fc
mustafa-tj <mustafatj7@gmail.com>
2022-11-13 02:14:02 +0530

    first commit

---
+++ dom.js
@@ -0,0 +1,31 @@
+// // Examiine the document object
+
+// // console.dir(document);
+// console.log(document.domain);
+// console.log(document.URL);
+// console.log(document.title);
+// // document.title = 123;
+// console.log(document.doctype);
+// console.log(document.head);
+// console.log(document.body);
+// console.log(document.all);
+// console.log(document.all[10]);
+// // document.all[10].textContent = 'Hello';
+// console.log(document.forms[0]);
+// console.log(document.links);
+// console.log(document.images);
+
+
+// Get elemment by ID //
+// console.log(document.getElementById('header-title'));
+// var headerTitle = document.getElementById('header-title');
+// var header = document.getElementById('main-header');
+// console.log(headerTitle);
+// headerTitle.textContent = 'Hello';
+// headerTitle.innerText = 'GoodBye.';
+// headerTitle.innerHTML = '<h3>Hello</h3>';
+// header.style.borderBottom = 'solid 3px black';
+
+// var addItems = document.getElementsByClassName('title');
+// addItems[0].style.fontWeight = 'bold';
+// addItems[0].style.color = 'green';
