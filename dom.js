1ecc45250bd383d588177066fbf6ab9117e94082
mustafa-tj <mustafatj7@gmail.com>
2022-11-17 01:45:08 +0530

    queryselector and queryselectorall

--- dom.js
+++ dom.js
@@ -54,11 +54,50 @@
 
 //getelementsbytagname//
 
-var li = document.getElementsByTagName('li');
-for(var i = 0; i < li.length ; i++){
-    li[i].style.backgroundColor = 'grey';
+// var li = document.getElementsByTagName('li');
+// for(var i = 0; i < li.length ; i++){
+//     li[i].style.backgroundColor = 'grey';
+// }
+
+//queryselector//
+var header = document.querySelector('#main-header');
+header.style.borderBottom = 'solid 4px #ccc'
+
+var input = document.querySelector('input');
+input.value = 'Hello World'
+
+var submit = document.querySelector('input[type="submit"]');
+submit.value = 'Send'
+
+var item = document.querySelector('.list-group-item');
+item.style.color = 'red';
+
+var secondItem = document.querySelector('.list-group-item:nth-child(2)');
+secondItem.style.backgroundColor = 'green';
+
+var lastItem = document.querySelector('.list-group-item:last-child');
+lastItem.style.color = 'white';
+
+
+
+//queryselectorall//
+
+var titles = document.querySelectorAll('.title');
+console.log(titles);
+titles[0].textContent = 'Hello';
+
+var itemss = document.querySelectorAll('.list-group-item');
+itemss[2].style.color = 'green';
+
+
+var odd = document.querySelectorAll('li:nth-child(odd)');
+
+for (var i = 0; i < odd.length; i++){
+    odd[i].style.backgroundColor = 'green';
 }
 
+var even = document.querySelectorAll('li:nth-child(even)');
 
-
-
+for (var i = 0; i < even.length; i++){
+    even[i].style.backgroundColor = 'pink';
+}
